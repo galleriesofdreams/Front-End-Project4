7c77befb0999465b3462cfcfc31109d261881889
Patrizia <patrizia.rohr@hotmail.com>
2021-05-06 10:55:11 +0100

    Installed Clean Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
@@ -12,5 +14,16 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
 }
