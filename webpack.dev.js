2582ed65a13f20a7ac9d631c14cd98ef8d96e180
Patrizia <patrizia.rohr@hotmail.com>
2021-05-06 11:16:13 +0100

    Sass installed

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,9 +9,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
