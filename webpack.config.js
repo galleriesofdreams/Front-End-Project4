34b7a5030f491b5625e3834410e3437e600915ac
galleriesofdreams <80752578+galleriesofdreams@users.noreply.github.com>
2021-05-07 14:10:11 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: 
+        [
+            new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new HtmlWebpackPlugin()
+    ]
+}
