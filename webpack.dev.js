fa3a926640d501159236f359cad7bfa747951e36
galleriesofdreams <80752578+galleriesofdreams@users.noreply.github.com>
2021-05-18 16:31:38 +0100

    Add files via upload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,31 +1,42 @@
-const path = require("path");
-const webpack = require("webpack");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+            },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
         ]
     },
     plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
             dry: true,
-            // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
